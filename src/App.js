9ff23b7a10c3d34287053c43247cc0292b301703
Tanmaya Vats <21ee01040@iitbbs.ac.in>
2024-02-27 20:56:28 +0530

    Uploaded Files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+// const expenses = [
+// 	{
+// 		id: "e1",
+// 		title: "Health ins",
+// 		amount: 143.99,
+// 		date: new Date(2023, 10, 23),
+// 	},
+// 	{
+// 		id: "e2",
+// 		title: "Car Insurance",
+// 		amount: 200.5,
+// 		date: new Date(2021, 8, 3),
+// 	},
+// 	{
+// 		id: "e3",
+// 		title: "ABCD",
+// 		amount: 234,
+// 		date: new Date(2025, 9, 12),
+// 	},
+// 	{
+// 		id: "e4",
+// 		title: "EFGH",
+// 		amount: 243.12,
+// 		date: new Date(2022, 4, 20),
+// 	},
+// ];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [expenses, setExpenses] = useState([]);
+
+	const addExpenseHandler = (expense) => {
+		console.log(expense);
+		setExpenses((prevExpenses) => {
+			return [expense, ...prevExpenses];
+		});
+	};
+
+	return (
+		<div>
+			<NewExpense onAddExpense={addExpenseHandler} />
+			<Expenses items={expenses} />
+		</div>
+	);
 }
 
 export default App;
